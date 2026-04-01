f7c6fa1b61d916507eefad0df7eec89f51822198
camilleepech <camille.pech@epfedu.fr>
2026-04-01 11:41:49 +0200

    [gpt5.3] Création d'une première page qui permet d'ajouter experience et skill
    [prompt]1.i want you to inspire from this architecture [xavier's coffee code] but to create a page that resemble a resume, where we can add experience, skills etc... start with something basic i will develop it later. i want main at least and another file if needed // (it was not working but because we didnt use fastapi dev)
    2.don't use jinja (so we could understand the code better)
    ---
    ( Finally, we used the code generated with jinja because we understood it and could run it when using fastapi dev)

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# In-memory storage
+experiences = []
+skills = []
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "experiences": experiences,
+            "skills": skills,
+        },
+    )
+
+@app.post("/add_experience")
+def add_experience(title: str = Form(...), company: str = Form(...)):
+    experiences.append({"title": title, "company": company})
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/add_skill")
+def add_skill(skill: str = Form(...)):
+    skills.append(skill)
+    return RedirectResponse(url="/", status_code=303)
